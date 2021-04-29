13dbb2b75a400ec7e70d1ace3947b5e353045adc
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-29 23:31:44 +0700

    refactor(lang): change default language to English (en-US)

--- config/config.ts
+++ config/config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
   },
   // https://umijs.org/zh-CN/plugins/plugin-locale
   locale: {
-    // default zh-CN
-    default: 'zh-CN',
+    // change default language to English (en-US)
+    default: 'en-US',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
