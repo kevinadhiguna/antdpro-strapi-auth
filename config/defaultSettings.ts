d4703b8c691e33fe5fe8552e07a57bd3f8f87dd4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-29 23:33:13 +0700

    chore(PWA): enable PWA

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -13,7 +13,7 @@ const Settings: LayoutSettings & {
   fixSiderbar: true,
   colorWeak: false,
   title: 'Ant Design Pro',
-  pwa: false,
+  pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
 };
