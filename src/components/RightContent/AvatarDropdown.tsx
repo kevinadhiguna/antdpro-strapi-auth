6452c1eb8ce10eb64f5227fdf56478adf65fb1a6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-30 09:00:37 +0700

    feat(name): change to username

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -97,8 +97,8 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
   return (
     <HeaderDropdown overlay={menuHeaderDropdown}>
       <span className={`${styles.action} ${styles.account}`}>
-        <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
-        <span className={`${styles.name} anticon`}>{currentUser.name}</span>
+        {/* <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" /> */}
+        <span className={`${styles.name} anticon`}>{username}</span>
       </span>
     </HeaderDropdown>
   );
