86c0432e3fb95bf6fda1acd45c417f6718d8b58d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-30 07:52:18 +0700

    feat(title): change title in login screen

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -133,7 +133,7 @@ const Login: React.FC = () => {
           <div className={styles.header}>
             <Link to="/">
               <img alt="logo" className={styles.logo} src="/logo.svg" />
-              <span className={styles.title}>Ant Design</span>
+              <span className={styles.title}>Ant Design Pro - Strapi Auth</span>
             </Link>
           </div>
           <div className={styles.desc}>
