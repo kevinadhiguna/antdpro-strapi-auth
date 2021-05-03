87aa9c180e10a5037e58378739ecb35ac54e27d3
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-03 17:08:33 +0700

    refactor(i18n): change layout title

--- src/locales/en-US/pages.ts
+++ src/locales/en-US/pages.ts
@@ -1,6 +1,6 @@
 export default {
   'pages.layouts.userLayout.title':
-    'Ant Design is the most influential web design specification in Xihu district',
+    'Ant Design Pro secured with Strapi GraphQL API 🔒',
   'pages.login.accountLogin.tab': 'Account Login',
   'pages.login.accountLogin.errorMessage': 'Incorrect username/password（admin/ant.design)',
   'pages.login.failure': 'Login failed, please try again!',
