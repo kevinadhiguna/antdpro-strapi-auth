0155914166e20fbe5f5a32bbb15e1d2135c1ddbd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-04 22:37:18 +0700

    feat(i18n): change input placeholder

--- src/locales/en-US/pages.ts
+++ src/locales/en-US/pages.ts
@@ -4,7 +4,7 @@ export default {
   'pages.login.accountLogin.errorMessage': 'Incorrect username/password（admin/ant.design)',
   'pages.login.failure': 'Login failed, please try again!',
   'pages.login.success': 'Login successful!',
-  'pages.login.username.placeholder': 'Username: admin or user',
+  'pages.login.username.placeholder': 'Username or Email address',
   'pages.login.username.required': 'Please input your username!',
   'pages.login.password.placeholder': 'Password: ant.design',
   'pages.login.password.required': 'Please input your password!',
