6ba3741c0c16122bea7bb71a0a872db60ed09d28
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-06 16:44:09 +0700

    refactor: able to render data in Table

--- src/pages/PlayerList/index.tsx
+++ src/pages/PlayerList/index.tsx
@@ -1,42 +1,83 @@
 import { Table } from 'antd';
 
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
+import { JUVENTUS } from '@/graphql/query';
+import { useQuery } from '@apollo/client';
 
 const PlayerList = () => {
-  return(
-    <Table dataSource={dataSource} columns={columns} />
+  let { loading, error, data } = useQuery(JUVENTUS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error !</p>;
+
+  console.info('Juventus Players :', data);
+
+  let size = Object.keys(data.juventuses).length;
+  console.log("Length of Data :", size);
+  
+  let dataArray: any[] = [];
+
+  for (let index = 0; index < size; index++) {
+    dataArray.push({
+      key: index,
+      name: data.juventuses[index].name,
+      number: data.juventuses[index].number,
+      age: data.juventuses[index].age,
+      country: data.juventuses[index].country,
+      appearences: data.juventuses[index].appearences,
+      goals: data.juventuses[index].goals,
+      minutesPlayed: data.juventuses[index].minutesPlayed,
+      position: data.juventuses[index].position,
+    })
+  }
+
+  console.info("Data Array : ", dataArray);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Number',
+      dataIndex: 'number',
+      key: 'number',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Country',
+      dataIndex: 'country',
+      key: 'country',
+    },
+    {
+      title: 'Appearence(s)',
+      dataIndex: 'appearences',
+      key: 'appearences',
+    },
+    {
+      title: 'Goal(s)',
+      dataIndex: 'goals',
+      key: 'goals',
+    },
+    {
+      title: 'Minutes Played',
+      dataIndex: 'minutesPlayed',
+      key: 'minutesPlayed',
+    },
+    {
+      title: 'Position',
+      dataIndex: 'position',
+      key: 'position',
+    },
+  ];
+
+  return (
+    <Table dataSource={dataArray} columns={columns} />
   );
-}
+};
 
 export default PlayerList;
