bb3e44a56c4e9689f798efca35a257a17d945919
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-07 19:02:22 +0700

    chore: add a necessarry description

--- src/pages/PlayerList/index.tsx
+++ src/pages/PlayerList/index.tsx
@@ -65,7 +65,7 @@ const PlayerList = () => {
       render: (record: any) => (
         // Log 'record' to console to see what is inside for debugging purpose
         <Avatar
-          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }} // <- Adjust avatars' responsiveness
           shape="circle"
           src={record}
           alt="juventus player"
