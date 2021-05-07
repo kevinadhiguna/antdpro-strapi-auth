7d10afb4926eb3312e1903c7a578371910debed9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-07 15:05:39 +0700

    refactor(avatar): remove width

--- src/pages/PlayerList/index.tsx
+++ src/pages/PlayerList/index.tsx
@@ -63,7 +63,10 @@ const PlayerList = () => {
       title: 'Avatar',
       dataIndex: 'avatar',
       width: 150,
-      render: (record: any) => <Avatar size="small" shape="circle" src={record} />, // <- Log record to console to see what is inside for debugging purpose.
+      render: (record: any) => (
+        // Log 'record' to console to see what is inside for debugging purpose
+        <Avatar size="small" shape="circle" src={record} alt="juventus player" />
+      ),
     },
     {
       title: 'Number',
