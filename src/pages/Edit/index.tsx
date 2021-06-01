31c2bf08fd4d0ae3f73233ec3c3d0bd2a604e2af
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-01 13:26:40 +0700

    feat: add an Edit screen

---
+++ src/pages/Edit/index.tsx
@@ -0,0 +1,194 @@
+import React, { useState } from 'react';
+import type { ProColumns } from '@ant-design/pro-table';
+import { EditableProTable } from '@ant-design/pro-table';
+import ProField from '@ant-design/pro-field';
+import { ProFormRadio } from '@ant-design/pro-form';
+import ProCard from '@ant-design/pro-card';
+
+const waitTime = (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+
+type DataSourceType = {
+  id: React.Key;
+  title?: string;
+  decs?: string;
+  state?: string;
+  created_at?: string;
+  children?: DataSourceType[];
+};
+
+const defaultData: DataSourceType[] = [
+  {
+    id: 624748504,
+    title: 'Activity One',
+    decs: 'This event is really fun',
+    state: 'open',
+    created_at: '2020-05-26T09:42:56Z',
+  },
+  {
+    id: 624691229,
+    title: 'Activity Two',
+    decs: 'This event is really amazing',
+    state: 'closed',
+    created_at: '2020-05-26T08:19:22Z',
+  },
+];
+
+export default () => {
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([]);
+  const [dataSource, setDataSource] = useState<DataSourceType[]>([]);
+  const [position, setPosition] = useState<'top' | 'bottom' | 'hidden'>('bottom');
+
+  const columns: ProColumns<DataSourceType>[] = [
+    {
+      title: 'Event Name',
+      dataIndex: 'title',
+      formItemProps: (form, { rowIndex }) => {
+        return {
+          rules: rowIndex > 2 ? [{ required: true, message: 'This is required' }] : [],
+        };
+      },
+      // Editing is not allowed on the second line
+      editable: (text, record, index) => {
+        return index !== 0;
+      },
+      width: '30%',
+    },
+    {
+      title: 'Status',
+      key: 'state',
+      dataIndex: 'state',
+      valueType: 'select',
+      valueEnum: {
+        all: { text: 'All', status: 'Default' },
+        open: {
+          text: 'Unsolved',
+          status: 'Error',
+        },
+        closed: {
+          text: 'Solved',
+          status: 'Success',
+        },
+      },
+    },
+    {
+      title: 'Description',
+      dataIndex: 'decs',
+      fieldProps: (from, { rowKey, rowIndex }) => {
+        if (from.getFieldValue([rowKey || '', 'title']) === '不好玩') {
+          return {
+            disabled: true,
+          };
+        }
+        if (rowIndex > 9) {
+          return {
+            disabled: true,
+          };
+        }
+        return {};
+      },
+    },
+    {
+      title: 'Activity Time',
+      dataIndex: 'created_at',
+      valueType: 'date',
+    },
+    {
+      title: 'Operation',
+      valueType: 'option',
+      width: 200,
+      render: (text, record, _, action) => [
+        <a
+          key="editable"
+          onClick={() => {
+            action?.startEditable?.(record.id);
+          }}
+        >
+          Edit
+        </a>,
+        <a
+          key="delete"
+          onClick={() => {
+            setDataSource(dataSource.filter((item) => item.id !== record.id));
+          }}
+        >
+          Delete
+        </a>,
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <EditableProTable<DataSourceType>
+        rowKey="id"
+        headerTitle="Editable Table"
+        maxLength={5}
+        recordCreatorProps={
+          position !== 'hidden'
+            ? {
+                position: position as 'top',
+                record: () => ({ id: (Math.random() * 1000000).toFixed(0) }),
+              }
+            : false
+        }
+        toolBarRender={() => [
+          <ProFormRadio.Group
+            key="render"
+            fieldProps={{
+              value: position,
+              onChange: (e) => setPosition(e.target.value),
+            }}
+            options={[
+              {
+                label: 'Add to the top',
+                value: 'top',
+              },
+              {
+                label: 'Add to the bottom',
+                value: 'bottom',
+              },
+              {
+                label: 'Hide',
+                value: 'hidden',
+              },
+            ]}
+          />,
+        ]}
+        columns={columns}
+        request={async () => ({
+          data: defaultData,
+          total: 3,
+          success: true,
+        })}
+        value={dataSource}
+        onChange={setDataSource}
+        editable={{
+          type: 'multiple',
+          editableKeys,
+          onSave: async () => {
+            await waitTime(2000);
+          },
+          onChange: setEditableRowKeys,
+        }}
+      />
+      <ProCard title="Tabular Data" headerBordered collapsible defaultCollapsed>
+        <ProField
+          fieldProps={{
+            style: {
+              width: '100%',
+            },
+          }}
+          mode="read"
+          valueType="jsonCode"
+          text={JSON.stringify(dataSource)}
+        />
+      </ProCard>
+    </>
+  );
+};
