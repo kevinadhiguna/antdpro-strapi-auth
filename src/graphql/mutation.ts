25ff11defb335a9e8502789c2b3328344303686b
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-04 13:14:12 +0700

    refactor(gql): add an UPLOADPROFPIC mutation

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -63,3 +63,29 @@ export const UPDATEJUVENTUS = gql`
 		}
 	}  
 `;
+
+// Upload a profile picture
+export const UPLOADPROFPIC = gql`
+	mutation UploadProfpic(
+		$refId: ID
+		$ref: String
+		$field: String
+		$file: Upload!
+	) {
+		upload(refId: $refId, ref: $ref, field: $field, file: $file) {
+			id
+			createdAt
+			updatedAt
+			alternativeText
+			caption
+			width
+			height
+			formats
+			hash
+			ext
+			mime
+			size
+			url
+		}
+	}
+`;
