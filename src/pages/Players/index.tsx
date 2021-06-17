5dee61e2f7d87972a0ffc5436a60403a81892d50
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-06-17 07:09:14 +0000

    refactor: define record as string instead of any

--- src/pages/Players/index.tsx
+++ src/pages/Players/index.tsx
@@ -62,7 +62,7 @@ const PlayerList: React.FC = () => {
     {
       title: 'Avatar',
       dataIndex: 'avatar',
-      render: (record: any) => (
+      render: (record: string) => (
         // Log 'record' to console to see what is inside for debugging purpose
         <Avatar
           size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }} // <- Adjust avatars' responsiveness
